4be784edda41adfa274edb0f3be0a0024545ac75
AlinaKhudokormova <alina950325@gmail.com>
2023-08-20 00:43:15 +0300

    Lesson 2

--- base.js
+++ base.js
@@ -1,3 +1,4 @@
+//1. INTRODUCTION TO JAVASCRIPT
 //Task 1
 const secMinute = 60;
 const secH = secMinute * 60;
@@ -25,3 +26,72 @@ const isAdult = confirm ("Are you of legal age?");
 
 //Task 5
 const myName = alert ("Alina");
+
+//2.OPERATORS. TYPE CONVERSIONS. ARRAYS. CONDITIONAL STATEMENT
+//Task 1 
+const validateAge = age => {
+  if (age>=18) {
+    console.log ("You are of legal age");
+  } else {
+    console.log ("You are too young");
+  }
+}
+//Task 2
+const filterMostOftenNumber = numsArr => {
+  // Create an object to store the frequency of each number
+  const numFrequency = {};
+
+  // Calculate the frequency of each number
+  numsArr.forEach(num => {
+    numFrequency[num] = (numFrequency[num] || 0) + 1;
+  });
+
+  // Find the most often included number and its frequency
+  let mostCommonNumber;
+  let maxFrequency = 0;
+
+  for (const num in numFrequency) {
+    if (numFrequency[num] > maxFrequency) {
+      mostCommonNumber = num;
+      maxFrequency = numFrequency[num];
+    }
+  }
+
+  // Create the new array without occurrences of the most common number
+  const filteredArray = numsArr.filter(num => num !== mostCommonNumber);
+
+  return filteredArray;
+}
+
+//Task 3
+const primeNumbers = numsArr => {
+  const newNumsArr = [];
+  for (const num of numsArr) {
+    if (num % 3 === 0 || num % 5 === 0 || num % 7 === 0 || num % 11 === 0 || num % 13 === 0) {
+      newNumsArr.push(num);
+    }
+  }
+  return newNumsArr;
+}
+//Task 4
+const triangleArea = (a, b, c) => {
+  if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+    return 'Incorrect data';
+  }
+  const semiPerimeter = (a + b + c) / 2;
+  const triangleArea = Math.sqrt((semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)));
+  return triangleArea.toFixed(3);
+}
+//Task 5
+const greetings = time => {
+  if (time >= 23 || time <= 5) {
+    return "Good night";
+  } else if (time >= 5 && time <= 11) {
+    return "Good morning";
+  } else if (time >= 11 && time <= 17) {
+    return "Good afternoon";
+  } else if (time >= 17 && time <= 23) {
+    return "Good evening";
+  }
+}
+
