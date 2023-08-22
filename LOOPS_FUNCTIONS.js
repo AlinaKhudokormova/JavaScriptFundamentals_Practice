29373cd1cd9b4e41dd5946114ef3f0158c052c54
AlinaKhudokormova <alina950325@gmail.com>
2023-08-23 00:32:37 +0300

    updated

--- LOOPS_FUNCTIONS.js
+++ LOOPS_FUNCTIONS.js
@@ -1,3 +1,4 @@
+/*
 //Task 1
 function createArray(start, end) {
   const resultArray = [];
@@ -7,8 +8,9 @@ function createArray(start, end) {
   return resultArray;
 }
 createArray();
-let arr = createArray(2, 9); //cheking
-console.log(arr); //cheking
+//cheking
+let arr = createArray(2, 9);
+console.log(arr);
 
 //Task 2
 function numbers(a, b) {
@@ -20,7 +22,8 @@ function numbers(a, b) {
   }
   console.log(allNumbers);
 }
-numbers(1, 8); //cheking
+//cheking
+numbers(1, 8);
 
 //Task 3
 function randArray(k) {
@@ -32,7 +35,63 @@ function randArray(k) {
   return resultArrayRandom;
 }
 randArray();
-console.log(randArray(6)); //cheking
+//cheking
+console.log(randArray(6));
 
 //Task 4
+function compact(arr) {
+  const filtredArr = [];
+  for (const i of arr) {
+    if (!filtredArr.includes(i)) {
+      filtredArr.push(i);
+    }
+  }
+  return filtredArr;
+}
+//cheking
+const arrTaskFour = [5, 3, 4, 5, 6, 7, 3];
+const arr2 = compact(arrTaskFour);
+console.log(arr2);
 
+//Task 5
+function funcName(arr) {
+  const numberArr = [];
+  const stringArr = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      numberArr.push(...item.filter(subItem => typeof subItem === 'number' && !isNaN(subItem)));
+      stringArr.push(...item.filter(subItem => typeof subItem === 'string'));
+    } else if (typeof item === 'number' && !isNaN(item)) {
+      numberArr.push(item);
+    } else if (typeof item === 'string') {
+      stringArr.push(item);
+    }
+  }
+
+  return [numberArr, stringArr];
+}
+//cheking
+const arr = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun'];
+const arrNew = funcN//chekingame(arr);
+console.log(arrNew);
+
+//Task 6
+function calc(a, b, op) {
+ let result;
+  if (op === 1){
+    result = a - b;
+  } else if(op === 2) {
+    result = a * b;
+  } else if (op === 3) {
+    result = a / b;
+  } else {
+    result = a + b;
+  }
+  return result;
+}
+//cheking
+console.log(calc(10, 3, 3));
+
+*/
+//Task 7
